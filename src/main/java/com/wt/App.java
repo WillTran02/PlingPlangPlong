b2384aa739aea86b134f778183ca7f7e9fb4516f
WillTran02 <williamqtran@hotmail.co.uk>
2022-07-08 11:00:18 +0100

    Setup logic and testing for simplest scenarios

--- src/main/java/com/wt/App.java
+++ src/main/java/com/wt/App.java
@@ -2,6 +2,13 @@ package com.wt;
 
 public class App {
     public static String playPlingPlangPlong(int number) {
+        if (number == 3) {
+            return "Pling";
+        } else if (number == 5) {
+            return "Plang";
+        } else if (number == 7) {
+            return "Plong";
+        }
         return "0";
     }
 }
--- src/test/java/com/wt/AppTest.java
+++ src/test/java/com/wt/AppTest.java
@@ -4,11 +4,30 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.wt.App.*;
 
 public class AppTest {
     @Test
     @DisplayName("check: entering 0 returns 0")
     void checkEntering0Returns0() {
-        assertEquals("0", App.playPlingPlangPlong(0));
+        assertEquals("0", playPlingPlangPlong(0));
+    }
+
+    @Test
+    @DisplayName("check: entering 3 returns \"Pling\"")
+    void checkEntering3ReturnsPling() {
+        assertEquals("Pling", playPlingPlangPlong(3));
+    }
+
+    @Test
+    @DisplayName("check: entering 5 returns \"Plang\"")
+    void checkEntering5ReturnsPlang() {
+        assertEquals("Plang", playPlingPlangPlong(5));
+    }
+
+    @Test
+    @DisplayName("check: entering 7 returns \"Plong\"")
+    void checkEntering7ReturnsPlong() {
+        assertEquals("Plong", playPlingPlangPlong(7));
     }
 }
